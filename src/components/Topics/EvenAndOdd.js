39ed07f4fde584c5dba3e7bb78fc69f56c48b5cc
jstntrn <justin.tran2290@gmail.com>
2019-01-11 09:48:56 -0700

    finished EvenAndOdd.js component

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 
 class EvenAndOdd extends Component {
+    constructor(props){
+        super(props)
+
+        this.splitArray = this.splitArray.bind(this)
+
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
+    }
+
+    updateInput(value) {
+        this.setState({
+            userInput: value
+        })
+    }
+
+    splitArray(str) {
+        let strArr = str.split(',');
+        let evenArr = strArr.filter((element) => element%2 === 0);
+        let oddArr = strArr.filter((element) => element%2 !== 0);
+        this.setState({
+            evenArray: evenArr,
+            oddArray: oddArr
+        })
+    }
+
+
     render() {
         return (
-            <div>
-                <p>EvenAndOdd Component</p>
-            </div>
+            <outline>
+                
+                <div className="puzzleBox evenAndOddPB">
+                    <h4>Evens and Odds</h4>
+                    <input className="inputLine"
+                        onChange = { (e) => this.updateInput(e.target.value) }
+                        value = {this.state.userInput}/>
+                    <button className="confirmationButton"
+                        onClick = { (e) => this.splitArray(this.state.userInput) }>Split</button>
+                    <span className="resultsBox">{`Evens: [${this.state.evenArray}]`}</span>
+                    <span className="resultsBox">{`Odds: [${this.state.oddArray}]`}</span>
+                </div>
+                
+            </outline>
+
+            
         );
     }
 }
